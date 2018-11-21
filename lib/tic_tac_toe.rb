2b05c8bf8d9a364e2ea59033f331dfa5f5de4422
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-21 06:33:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,14 +10,18 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
 ]
 
-# display the board
-# display the board
+# prints the current board representation, 1 argument
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+  return index
 end
 
 def turn_count(board)
@@ -45,10 +49,7 @@ def move(board, index, value)
   return board
 end
 
-def input_to_index(input)
-  index = input.to_i - 1
-  return index
-end
+
 
 def valid_move?(board, index)
   if index.between?(0, 8) && position_taken?(board, index) == false
