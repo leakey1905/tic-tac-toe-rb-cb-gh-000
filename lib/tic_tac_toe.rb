5bb73a0f6e80dc2df4bfcaadf724d957a8965d36
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-21 06:32:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,6 +10,16 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
 ]
 
+# display the board
+# display the board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def turn_count(board)
   cnt = 9
   board.each do |pos|
@@ -27,15 +37,6 @@ def current_player(board)
   else
     return "O"
   end
-end
-
-# display the board
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # move the things
